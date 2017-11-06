37ff472eb5febfa73352391eac6fa58327f95bf0
eagle-shop <22486435+eagle-shop@users.noreply.github.com>
2017-11-07 00:44:12 +0900

    Initial Commit

---
+++ DeckBuildCardGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckBuildCardGame
+//
+//  Created by masato on 2017/11/07.
+//  Copyright © 2017年 eagle-shop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckBuildCardGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckBuildCardGame
+//
+//  Created by masato on 2017/11/07.
+//  Copyright © 2017年 eagle-shop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
